bc0e2fcb5b60186b7c660de659242ee92ab6d556
TheNCf <68508480+TheNCf@users.noreply.github.com>
2021-02-19 21:54:39 +0300

    initial commit

--- src/components/Searchpage/Searchpage.jsx
+++ src/components/Searchpage/Searchpage.jsx
@@ -39,36 +39,36 @@ let colors = [
 ];
 
 function Searchpage({getAllModels, modelsdata}) {
-
-    const allItems = modelsdata.models;
-    const [itemsPerPage, setItemsPerPage] = useState(2);
-    const [pages, setPages] = useState();
-    const [currentPage, setCurrentPage] = useState(1);
-
-    useEffect(() => {
-        getAllModels();
-        setPages(Math.ceil(allItems.length/itemsPerPage));
+    
+    useEffect(() => {getAllModels();
+        // setPages(Math.ceil(allItems.length/itemsPerPage));
+        // let items = [];
+        // for (let i = currentPage * itemsPerPage - itemsPerPage; i < currentPage * itemsPerPage; i++) {
+        //     items.push(allItems[i]);
+        // }
     }, [getAllModels]);
+    const allItems = modelsdata.models;
+    console.log(allItems);
+    // const [itemsPerPage, setItemsPerPage] = useState(2);
+    // const [pages, setPages] = useState();
+    // const [currentPage, setCurrentPage] = useState(1);
+    //const [itemElements, setItemElements] = useState();
+    let itemElements = (allItems.map(i => <Itemcard itemname={i.name} price={i.price + " руб."} discount={i.discount} img={i.img} margin="0 0 10px auto" key={i.name + i.price + i.discount} />));
+//console.log(pages)
+    // let pagesAr = [];
+    // for (let i = 0; i < pages; i++) {
+    //     pagesAr[i] = i + 1;
+    // }
 
-    let pagesAr = [];
-    for (let i = 0; i < pages; i++) {
-        pagesAr[i] = i + 1;
-    }
-
-    let items = [];
-    for (let i = currentPage * itemsPerPage - itemsPerPage; i < currentPage * itemsPerPage; i++) {
-        items.push(allItems[i]);
-    }
-
-    const updateCurrentPage = (value) => {
-        setCurrentPage(value);
-    }
+    // const updateCurrentPage = (value) => {
+    //     setCurrentPage(value);
+    // }
 
     let positionElements = positions.map(p => <p className={css.groupposition}>{p}</p>);
     let sizeElements = sizes.map(s => <Checkbox name="size" caption={s} value={s}/>);
-    let colorElements = colors.map(c => <Colorprobe color={c} name="color"/>);
-    let itemElements = items.map(i => <Itemcard itemname={i.name} price={i.price + " руб."} discount={i.discount} img={i.img} margin="0 0 10px auto" />);
-    let pageElements = pagesAr.map(p => <Circlebutton caption={p} value={p} onClick={updateCurrentPage} />);
+    let colorElements = colors.map(c => <Colorprobe color={c} name="color" key={c} />);
+    
+    //let pageElements = pagesAr.map(p => <Circlebutton caption={p} value={p} onClick={updateCurrentPage} key={p} />);
     return(
         <div className={css.content + " container"}>
             <div className={css.searchbarcontainer}>
@@ -96,7 +96,7 @@ function Searchpage({getAllModels, modelsdata}) {
             <div className={css.listofcontents}>
                 {itemElements}
                 <div className={css.pagebuttons}>
-                    <div className={css.pagebuttonscontainer}>{pageElements}</div>
+                    <div className={css.pagebuttonscontainer}>{/*pageElements*/}</div>
                 </div>
             </div>
         </div>
